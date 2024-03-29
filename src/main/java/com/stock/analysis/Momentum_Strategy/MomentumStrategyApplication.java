12f92795df654fe2c60819de3b29ad6a78dc9e3d
Tushar Desarda <tushardesarda@outlook.com>
2024-03-29 12:56:53 +0530

    Initial commit

---
+++ src/main/java/com/stock/analysis/Momentum_Strategy/MomentumStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.stock.analysis.Momentum_Strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentumStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentumStrategyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock/analysis/Momentum_Strategy/MomentumStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.analysis.Momentum_Strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentumStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
