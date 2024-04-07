f9198ccc54b1807ba3ec4c8a1bebab64e1636fcc
Tushar Desarda <tushardesarda@outlook.com>
2024-04-07 23:23:59 +0530

    return calculation formula modified

--- src/main/java/com/stock/analysis/Momentum_Strategy/Util/ReturnCalculationUtils.java
+++ src/main/java/com/stock/analysis/Momentum_Strategy/Util/ReturnCalculationUtils.java
@@ -1,8 +1,13 @@
 package com.stock.analysis.Momentum_Strategy.Util;
 
+import java.text.DecimalFormat;
+
 public class ReturnCalculationUtils {
 
     public static float  percentReturn(Float startingPrice,Float closingPrice){
-        return (closingPrice/startingPrice)*100;
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
+        float priceReturn=((closingPrice-startingPrice)/startingPrice)*100;
+        return Float.valueOf(df.format(priceReturn));
     }
 }
